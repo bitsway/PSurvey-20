c2d2d830a3a7bf5eb90b2fd701f9913c5f208bbc
nadira.nuri <nadira.nuri@gmail.com>
2017-03-14 09:56:47 +0600

    t 3 20170314

--- www/survey.js
+++ www/survey.js
@@ -2525,9 +2525,9 @@ function prescription_submit(){
 			var imageName=localStorage.user_id+'_'+now.toString()+'.jpg';
 			//alert (imageName);
 			if (medicine_3==''){		
-				$("#error_prescription_submit").text("Required medicine_3");
-				$("#wait_image_prescription").show();
-				$("#btn_prescription_submit").hide();	
+				$("#error_prescription_submit").text("Required Doctor Catagory:");
+				$("#wait_image_prescription").hide();
+				$("#btn_prescription_submit").show();	
 			}else{
 				$("#error_prescription_submittxt").val(localStorage.base_url+'prescription_submit?cid='+localStorage.cid+'&rep_id='+localStorage.user_id+'&rep_pass='+encodeURIComponent(localStorage.user_pass)+'&synccode='+localStorage.synccode+'&areaId='+areaId+'&doctor_id='+encodeURIComponent(doctorId)+'&doctor_name='+encodeURIComponent(doctor_name)+'&latitude='+latitude+'&longitude='+longitude+'&pres_photo='+imageName+'&campaign_doc_str='+localStorage.campaign_doc_str+'&medicine_1='+medicine_1+'&medicine_2='+medicine_2+'&medicine_3='+medicine_3+'&medicine_4='+medicine_4+'&medicine_5='+medicine_5+'&checkOther='+checkOther)							
 
